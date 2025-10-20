978bf9cba4fb6465c830b35a77318fe52a050ace
Bruno Gomes <bgomes.89@gmail.com>
2025-10-20 17:16:51 -0300

    fix: config host

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import { app } from './app.ts'
 import { env } from './env/index.ts'
 
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ 
+  host: '0.0.0.0',
+  port: env.PORT
+ }).then(() => {
   console.log(`Server is running on http://localhost:${env.PORT}`)
 })
